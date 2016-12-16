e7ea60b1fa718ec2265669d81ce4b584aadbcd53
Tércio Garcia <tercio.garcia@smarti9.com.br>
2016-12-16 07:18:00 -0200

    Configurando porta

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const mongoose = require('./config/mongoose');
 const express = require('./config/express');
 var db = mongoose();
 var app = express();
-app.listen(3000);
+if(process.env.NODE_ENV == 'production'){
+	app.listen(process.env.PORT);
+}
+else{
+	app.listen(3000);
+}
 console.log("Servidor executando em http://localhost:3000/...");
 
 module.exports = app;
